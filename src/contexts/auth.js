f53b00c6efaa7059221890e3d0bbf4b4e18dc495
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-03-22 14:33:25 -0300

    Implements authentication Context

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,66 +1,16 @@
 // dependencies
-import { createContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { createContext } from "react";
+
+// hooks
+import useAuth from "../hooks/useAuth";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const navigate = useNavigate();
 
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const recoveredUser = localStorage.getItem('user');
-
-        if (recoveredUser) {
-            setUser(JSON.parse(recoveredUser));
-        }
-
-        // to avoid page loading without properly gathering the user info from localStorage, we must use a state to wait for it. When the data fetch is ended, then we set Loading to false and then we render the page (this last one is made on Routes file)
-        setLoading(false);
-    }, []);
-
-    const login = async (email, password) => {
-        // console.log('login auth', { email, password });
-
-        // call api
-        const response = await fetch('/api/login', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password })
-        });
-
-        const body = await response.json();
-
-        if (response.status === 403) {
-            alert("Email ou senha incorretos!")
-        }
-
-
-        // creating a session api
-        const loggedUser = {
-            id: body.id,
-            email: body.email
-        };
-
-        // saving user on localStorage
-        localStorage.setItem('user', JSON.stringify(loggedUser));
-
-        if (body.email) {
-            setUser(loggedUser);
-            navigate('/home');
-        }
-    };
-
-    const logout = () => {
-        console.log('logout');
-        localStorage.removeItem('user');
-        setUser(null);
-        navigate('/');
-    };
+    const {
+        user, loading, login, logout
+    } = useAuth();
 
     // !!user:
     // user != null, then authenticated = true
---
+++ src/hooks/useAuth.js
@@ -0,0 +1,46 @@
+// dependencies
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import api from "../api"
+
+export default function useAuth() {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const recoveredToken = localStorage.getItem('token');
+    const recoveredUser = localStorage.getItem('user');
+
+    if (recoveredToken) {
+      api.defaults.headers.Authorization = `Bearer ${JSON.parse(recoveredToken)}`;
+      setUser(recoveredUser);
+    }
+
+    // to avoid page loading without properly gathering the user info from localStorage, we must use a state to wait for it. When the data fetch is ended, then we set Loading to false and then we render the page (this last one is made on Routes file)
+    setLoading(false);
+  }, []);
+
+  const login = async (email, password) => {
+    // call api
+    const { data: { accessToken, user } } = await api.post("/api/login", { email, password });
+
+    localStorage.setItem("token", JSON.stringify(accessToken));
+    localStorage.setItem("user", JSON.stringify(user));
+    api.defaults.headers.Authorization = `Bearer ${accessToken}`;
+    setUser(user);
+    navigate('/home');
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    api.defaults.headers.Authorization = undefined;
+    navigate('/');
+  };
+
+  return { user, loading, login, logout }
+}
