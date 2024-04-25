2a9fb17654a92f1167a166a556f628bd9692bdde
Jefferson Pedro <jeffersonpedro89@gmail.com>
2024-04-24 22:43:15 -0300

    Add task for seed db

--- cypress.config.js
+++ cypress.config.js
@@ -4,16 +4,30 @@ const {
   saveUsersSeed,
 } = require("./scripts/generateSeedUsers");
 
+const { default: axios } = require("axios");
+
 module.exports = defineConfig({
   projectId: "8jd3re",
   env: {
-    api_server: "http://localhost:9000",
+    apiUrl: "localhost:9000",
   },
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const { apiUrl } = config.env;
+
       on("task", {
+        async "db:seed"() {
+          const user = createSeedUsers(1)[0];
+
+          const { data } = await axios.post(
+            `https://${apiUrl}/api/tutors`,
+            user,
+          );
+
+          return data ? user : false;
+        },
+
         createUser() {
           const user = createSeedUsers(1)[0];
           return user;
